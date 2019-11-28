fda2e56f16f07e23fc9003bacb6704688d12a1e8
pedry98 <50678147+pedry98@users.noreply.github.com>
2019-11-28 13:18:32 -0500

    Add files via upload

---
+++ CheckPalindrom/Form1.Designer.cs
@@ -0,0 +1,99 @@
+﻿namespace CheckPalindrom
+{
+	partial class Form1
+	{
+		/// <summary>
+		/// Required designer variable.
+		/// </summary>
+		private System.ComponentModel.IContainer components = null;
+
+		/// <summary>
+		/// Clean up any resources being used.
+		/// </summary>
+		/// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+		protected override void Dispose(bool disposing)
+		{
+			if (disposing && (components != null))
+			{
+				components.Dispose();
+			}
+			base.Dispose(disposing);
+		}
+
+		#region Windows Form Designer generated code
+
+		/// <summary>
+		/// Required method for Designer support - do not modify
+		/// the contents of this method with the code editor.
+		/// </summary>
+		private void InitializeComponent()
+		{
+			this.textBox1 = new System.Windows.Forms.TextBox();
+			this.lblInput = new System.Windows.Forms.Label();
+			this.lblOutput = new System.Windows.Forms.Label();
+			this.button1 = new System.Windows.Forms.Button();
+			this.SuspendLayout();
+			// 
+			// textBox1
+			// 
+			this.textBox1.Font = new System.Drawing.Font("Lucida Sans Unicode", 16.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+			this.textBox1.Location = new System.Drawing.Point(50, 92);
+			this.textBox1.Name = "textBox1";
+			this.textBox1.Size = new System.Drawing.Size(457, 49);
+			this.textBox1.TabIndex = 0;
+			// 
+			// lblInput
+			// 
+			this.lblInput.AutoSize = true;
+			this.lblInput.Font = new System.Drawing.Font("Lucida Sans Unicode", 16.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+			this.lblInput.Location = new System.Drawing.Point(47, 46);
+			this.lblInput.Name = "lblInput";
+			this.lblInput.Size = new System.Drawing.Size(442, 34);
+			this.lblInput.TabIndex = 1;
+			this.lblInput.Text = "Enter text to check if palindrom";
+			// 
+			// lblOutput
+			// 
+			this.lblOutput.AutoSize = true;
+			this.lblOutput.Font = new System.Drawing.Font("Lucida Sans Unicode", 16.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+			this.lblOutput.Location = new System.Drawing.Point(47, 225);
+			this.lblOutput.Name = "lblOutput";
+			this.lblOutput.Size = new System.Drawing.Size(0, 34);
+			this.lblOutput.TabIndex = 2;
+			// 
+			// button1
+			// 
+			this.button1.Font = new System.Drawing.Font("Lucida Sans Unicode", 16.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+			this.button1.Location = new System.Drawing.Point(179, 289);
+			this.button1.Name = "button1";
+			this.button1.Size = new System.Drawing.Size(232, 96);
+			this.button1.TabIndex = 3;
+			this.button1.Text = "Check";
+			this.button1.UseVisualStyleBackColor = true;
+			this.button1.Click += new System.EventHandler(this.button1_Click);
+			// 
+			// Form1
+			// 
+			this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+			this.ClientSize = new System.Drawing.Size(800, 450);
+			this.Controls.Add(this.button1);
+			this.Controls.Add(this.lblOutput);
+			this.Controls.Add(this.lblInput);
+			this.Controls.Add(this.textBox1);
+			this.Name = "Form1";
+			this.Text = "Form1";
+			this.ResumeLayout(false);
+			this.PerformLayout();
+
+		}
+
+		#endregion
+
+		private System.Windows.Forms.TextBox textBox1;
+		private System.Windows.Forms.Label lblInput;
+		private System.Windows.Forms.Label lblOutput;
+		private System.Windows.Forms.Button button1;
+	}
+}
+
---
+++ CheckPalindrom/Form1.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace CheckPalindrom
+{
+	public partial class Form1 : Form
+	{
+		String input;
+		String inputnospaces;
+		public Form1()
+		{
+			InitializeComponent();
+		}
+
+		private void button1_Click(object sender, EventArgs e)
+		{
+			input = textBox1.Text;
+			
+			foreach(char a in input)
+				{
+					if(a != ' ')
+					{
+						inputnospaces += a;
+					}
+				}
+			
+				
+				
+
+			if (CheckPalindrom(inputnospaces) == true)
+				lblOutput.Text = "its a palindrom";
+			else
+				lblOutput.Text = "Sorry it isnt";
+		}
+
+
+
+
+
+		private bool CheckPalindrom(String input)
+		{
+			String temp = "";
+			for(int x = input.Length-1; x >= 0; x--)
+			{
+				temp += input.Substring(x, 1);
+			}
+			if (input.Equals(temp))
+				return true;
+			else
+				return false;
+		}
+	}
+}
